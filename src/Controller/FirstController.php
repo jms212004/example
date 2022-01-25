a7bdcb2cdfef86a78aa8f846bfb2da92a963e252
jms <jms212004@gmail.com>
2022-01-25 15:06:19 +0100

    Passer une valeur de l url vers le controleur

--- src/Controller/FirstController.php
+++ src/Controller/FirstController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -19,8 +20,9 @@ class FirstController extends AbstractController
     }
 
     #[Route('/sayHello/{firstname}/{lastname}', name: 'say.hello')]
-    public function sayHello($firstname,$lastname): Response
+    public function sayHello(Request $request, $firstname,$lastname): Response
     {
+        dd($request);
         return $this->render('first/hello.html.twig', [
             'controller_name' => 'FirstController',
             'firstname' => $firstname,
