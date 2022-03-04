15b217e311b31b7698935e67913228b2f9af4f34
jms <jms212004@gmail.com>
2022-03-04 14:43:03 +0100

    utilisation des traits

--- src/Entity/Personne.php
+++ src/Entity/Personne.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\PersonneRepository;
+use App\Traits\TimeStampTrait;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
@@ -13,6 +14,8 @@ use Doctrine\Persistence\Event\LifecycleEventArgs;
 
 class Personne
 {
+    use TimeStampTrait;
+    
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer')]
@@ -136,41 +139,5 @@ class Personne
         $this->job = $job;
 
         return $this;
-    }
-
-    public function getCreatedAt(): ?\DateTimeImmutable
-    {
-        return $this->createdAt;
-    }
-
-    public function setCreatedAt(?\DateTimeImmutable $createdAt): self
-    {
-        $this->createdAt = $createdAt;
-
-        return $this;
-    }
-
-    public function getUpdatedAt(): ?\DateTimeInterface
-    {
-        return $this->updatedAt;
-    }
-
-    public function setUpdatedAt(?\DateTimeInterface $updatedAt): self
-    {
-        $this->updatedAt = $updatedAt;
-
-        return $this;
-    }
-
-    #[ORM\PrePersist]
-    public function onPrePersist() {
-        $this->createdAt = new \DateTime('NOW');
-        $this->updatedAt = new \DateTime('NOW');
-    }
-
-
-    #[ORM\PreUpdate]
-    public function onPreUpdate() {
-        $this->updatedAt = new \DateTime('NOW');
     }
 }
