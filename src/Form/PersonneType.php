c0deda69feb24c9c1fa13366ce54a6366656f452
jms <jms212004@gmail.com>
2022-03-06 11:03:26 +0100

    modifier l affichage des champs dans le formulaire

--- src/Form/PersonneType.php
+++ src/Form/PersonneType.php
@@ -3,10 +3,14 @@
 namespace App\Form;
 
 use App\Entity\Personne;
+use App\Entity\Profile;
+use App\Entity\Hobby;
+use Doctrine\ORM\EntityRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class PersonneType extends AbstractType
 {
@@ -18,8 +22,20 @@ class PersonneType extends AbstractType
             ->add('age')
             ->add('createdAt')
             ->add('updatedAt')
-            ->add('profile')
-            ->add('hobbies')
+            ->add('profile', EntityType::class, [
+                'expanded' => true,
+                'class' => Profile::class,
+                'multiple' => false]
+            )
+            ->add('hobbies', EntityType::class, [
+                'expanded' => false,
+                'class' => Hobby::class,
+                'multiple' => true,
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('h')
+                        ->orderBy('h.designation', 'ASC');
+                }
+            ])
             ->add('job')
             ->add('editer', SubmitType::class)
         ;
