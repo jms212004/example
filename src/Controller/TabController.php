bd3e4f052dd236d4e85e30b9963d610a997aae0a
jms <jms212004@gmail.com>
2022-03-27 11:04:51 +0200

    version ok creation user

--- src/Controller/TabController.php
+++ src/Controller/TabController.php
@@ -3,10 +3,14 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\UserType;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use DateTimeImmutable;
 
 //factoriser l uri et limiter acces au role pour tout le controler
 #[
@@ -27,16 +31,54 @@ class TabController extends AbstractController
         ]);
     }
 
-    #[Route('/users', name: 'tab.users')]
-    public function users(): Response
+    #[Route('/edit/{id?0}', name: 'tab.edit')]
+    public function addUsers(
+        User $user = null,
+        ManagerRegistry $doctrine,
+        UserPasswordHasherInterface $userPasswordHasher,
+        Request $request
+    ): Response
     {
-        $users = [
-            ['firstname' => 'jm', 'name' => 's', 'age' => '22'],
-            ['firstname' => 'jm2', 'name' => 's2', 'age' => '23'],
-            ['firstname' => 'jm3', 'name' => 's3', 'age' => '24']
-        ];
-        return $this->render('tab/users.html.twig', [
-            'users' => $users
+
+        // instancier la classe personne
+        $user = new User();
+
+        // creation des champs du formulaire a partir de la classe personne
+        $form = $this->createForm(UserType::class, $user);
+        
+        // Mon formulaire va  traiter la requete
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // encode the plain password
+            $user->setPassword(
+                $userPasswordHasher->hashPassword(
+                        $user,
+                        $form->get('password')->getData()
+                    )
+                );
+
+            // Set their role
+            $user->setRoles(['ROLE_ADMIN']);
+
+            //set date update et create
+            $now = new DateTimeImmutable();
+            $user->setUpdatedAt($now);
+            $user->setCreatedAt($now);
+
+            // on va ajouter l'objet personne dans la base de données
+            $manager = $doctrine->getManager();
+            $manager->persist($user);
+            $manager->flush();
+
+            // Rediriger verts la liste des utilisateurs
+            return $this->redirectToRoute('tab.listuser');
+        }
+
+
+        // affichage des informations dans la page detail
+        return $this->render('tab/add-utilisateurs.html.twig', [
+            'formuser' => $form->createView()//view
         ]);
     }
 
